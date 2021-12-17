e822873ee108d1866e2afe732a446475002c92a8
CasualEd <94349386+CasualEd@users.noreply.github.com>
2021-12-17 08:37:49 +0100

    Question 3

--- main.c
+++ main.c
@@ -9,34 +9,48 @@
 int main(int argc, char * argv){
 	int pid, status;
 	char command[bufSizeMax];
+	int cmp;
 	
 	char * welcomeMessage="Welcome to ENSEA Tiny Shell. \nPour quitter, tapez 'exit'. ";
-	const char * shellMessage="\nenseash \% "; 	
+	const char * shellMessage="\nenseash \% \n "; 	
 	int lgthWelcString=strlen(welcomeMessage); // Since each character is represent by 8 bits we multiply by 8 the size of buffer
 	int lgthShellString=strlen(shellMessage);
+	const char * exitMessage="Bye bye... \n ";
+	int lgthByeString=strlen(exitMessage);
 	
 	write(STDOUT_FILENO, welcomeMessage, lgthWelcString); // We print the welcome message string directly on the screen
 	
 	while(1){ 					//never-ending loop that creates a child after each command excuted to execute it, and ask for a new command after its termination
 		pid = fork();
-		if(pid!=0){
+		if(pid!=0){				//father's code
+			//if(cmp==0){exit(EXIT_SUCCESS);}
 			wait(&status);
-		}else{
-	
+			if(WEXITSTATUS(status)==2){
+				write(STDOUT_FILENO, exitMessage, lgthShellString);
+				exit(EXIT_SUCCESS);
+			}
+		}else{					//child's code
+			
 			int readLength;
-			int writeLength;
 			int retrn;
+			//int writeLength;	
+			
 			write(STDOUT_FILENO, shellMessage, lgthShellString);
 			readLength=read(STDIN_FILENO,command,bufSizeMax);
 			*(command+readLength-1) ='\0';
-	
-		retrn = execlp(command,command, (char  *) NULL);
-		printf("%d",retrn); //prints -1 if there is an execution error
-		exit(retrn);
+			cmp = strncmp(command,"exit",bufSizeMax);
+			
+			if(cmp==0){
+				exit(2);
+			}else{
+				retrn = execlp(command,command, (char  *) NULL);
+				printf("%d",retrn); //prints -1 if there is an execution error
+				exit(retrn);
+			}
 		}
 	}
 	
-}// EXIT_SUCCESS
+}
 
 //ShellMess
 	//const char * shellMessage="\nenseash \%"; 	
