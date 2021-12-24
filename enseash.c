4ea861d396e16346fbe737752c9d4a89a5960dd6
CasualEd <94349386+CasualEd@users.noreply.github.com>
2021-12-24 18:02:28 +0100

    Question 7(version finale)

--- enseash.c
+++ enseash.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdlib.h> 	//for exit()
 #include <time.h> //for the clock
+#include <fcntl.h> //for open()
 #include <wait.h>
 #include <signal.h>
 #define bufSizeMax 1024
@@ -19,7 +20,7 @@ void printString(char * string){
 	write(STDOUT_FILENO, string, lgthString);
 }
 
-void getCommandEnd(char * command, char * comExec, char ** argument_list){
+void getCommandEnd(char * command, char * comExec, char ** argument_list, int * flagStdIO, char * filestdout, char * filestdin){
 	//reading the whole command
 	int readLength;
 	readLength=read(STDIN_FILENO,command,bufSizeMax);
@@ -29,10 +30,21 @@ void getCommandEnd(char * command, char * comExec, char ** argument_list){
 	char *token;
 	token = strtok(command, " ");
 	sprintf(comExec,"%s", token); //retrieving the command's name
-	
+
 	int argpos=0;
 	while(token != NULL){ //retrieving and storing parameters in an array
 		sprintf(argument_list[argpos],"%s", token);
+		if(strncmp(argument_list[argpos], ">", bufSizeMax)==0){
+			*flagStdIO=1;
+			argument_list[argpos]=NULL;
+			token = strtok(NULL, " ");
+			sprintf(filestdout,"%s", token);
+		}else if(strncmp(argument_list[argpos], "<", bufSizeMax)==0){
+			*flagStdIO=2;
+			argument_list[argpos]=NULL;
+			token = strtok(NULL, " ");
+			sprintf(filestdin,"%s", token);
+		}
 		argpos++;
 		token = strtok(NULL, " ");
 	}
@@ -40,7 +52,7 @@ void getCommandEnd(char * command, char * comExec, char ** argument_list){
 	free(token);
 }
 
-void getCommand(char * command, char * comExec, char ** argument_list, int flag, int old_status, int sigOrExit, long timeDiff){
+void getCommand(char * command, char * comExec, char ** argument_list, int flag, int old_status, int sigOrExit, long timeDiff, int * flagStdIO, char * filestdout, char * filestdin){
 	if(flag==0){			//first command typed in
 		printString("enseash % ");
 	}else{ //at least one command has been entered
@@ -55,7 +67,7 @@ void getCommand(char * command, char * comExec, char ** argument_list, int flag,
 		printString(strInfo);
 		free(strInfo);
 	}
-	getCommandEnd(command, comExec, argument_list);
+	getCommandEnd(command, comExec, argument_list, flagStdIO, filestdout, filestdin);
 }
 
 
@@ -81,15 +93,22 @@ int main(int argc, char ** argv){
 	while(1){ //never-ending loop that creates a child after each command executed to execute it, and ask for a new command after its termination
 		//initialisation of an array of string to store the command's parameters
 		//done at each turn of loop to avoid segmentation faults
-		//freed below
+		//freed at the end of the while
 		char ** argument_list = malloc(sizeof(char*)*bufInt);
 		for(int im=0;im<bufInt;im++){
 			argument_list[im]= (char*)malloc(bufSizeMax*sizeof(char));
 		}
 		char * comExec = malloc(sizeof(char)*bufSizeMax); //string to save the name of the command called
+		int * flagStdIO= malloc(sizeof(int)); //takes 1 if needed to redirect to a file, 2 if from a file
+		*flagStdIO = 0;
+		char * filestdout = malloc(sizeof(char)*bufSizeMax); //to store the output filename
+		char * filestdin = malloc(sizeof(char)*bufSizeMax); //to store the output filename
+		
+		
 		
 		timeDiff=timeDifferencems(timeStart,timeStop);		//getting the execution time in ms
-		getCommand(command, comExec, argument_list, flag, old_status, sigOrExit, timeDiff);
+		getCommand(command, comExec, argument_list, flag, old_status, sigOrExit, timeDiff, flagStdIO, filestdout, filestdin);
+		
 		if(strncmp(command,"exit",bufSizeMax)==0){ //the program exits if exit is typed
 			printString("Bye bye...\n");
 			exit(EXIT_SUCCESS);
@@ -108,13 +127,22 @@ int main(int argc, char ** argv){
 			}
 			flag=1; //to mark that one command has been executed
 		}else{			//child's code
+			if(*flagStdIO==1){ //redirections to stdin and stdout
+				int fdout = open(filestdout, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+				dup2(fdout, 1);   // make stdout go to file
+				close(fdout);
+			}else if(*flagStdIO==2){
+				int fdin = open(filestdin, O_RDONLY);
+				dup2(fdin, 0);   // make file go to stdin
+				close(fdin);
+			}
 			execvp(comExec, argument_list);
 			printString("Erreur d'exécution, veuillez revoir votre commande.\n");
 			exit(EXIT_FAILURE);
 		}
 		free(argument_list);
 		free(comExec);
+		free(filestdout);
+		free(flagStdIO);
 	}	
 }
-
-
